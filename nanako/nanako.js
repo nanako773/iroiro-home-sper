26e5a63c4638cde01e4815de1a05d15617e832e5
nanako773 <163623198+nanako773@users.noreply.github.com>
2025-08-31 22:39:45 +0900

    Update website content

--- nanako/nanako.js
+++ nanako/nanako.js
@@ -45,35 +45,3 @@ document.addEventListener('touchmove', resetInactivityTimer);
 
 // 最初のタイマーを開始
 resetInactivityTimer();
-
-
-
-// Apps ScriptでデプロイしたウェブアプリのURL
-        // YOUR_WEB_APP_URLをコピーしたURLに置き換えてください
-        const apiUrl = 'https://script.google.com/macros/s/AKfycby-WANipTuihhGTu7OvhSCvnT90eMp8CpJPi4PpkunLnlgSt07fQ-v3MGmMeE57lBkU/exec';
-
-        document.addEventListener('DOMContentLoaded', () => {
-            const currentVersionElement = document.querySelector('.current-version');
-            if (!currentVersionElement) return;
-
-            const currentVersion = currentVersionElement.textContent.trim();
-            
-            fetch(apiUrl)
-                .then(response => {
-                    if (!response.ok) {
-                        throw new Error('ネットワーク応答が正常ではありませんでした');
-                    }
-                    return response.json();
-                })
-                .then(data => {
-                    const latestVersion = data.latest_version;
-                    
-                    if (currentVersion !== latestVersion) {
-                        // バージョンが一致しない場合に通知を表示
-                        alert(`新しいバージョンが利用可能です！\n現在のバージョン: ${currentVersion}\n最新バージョン: ${latestVersion}`);
-                    }
-                })
-                .catch(error => {
-                    console.error('バージョン情報の取得中にエラーが発生しました:', error);
-                });
-             });
